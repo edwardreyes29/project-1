58730403f253cb7177dc366d9dde9c74aba33aec
Nick Guimarais <Nick.guimarais@gmail.com>
2020-08-06 14:27:10 -0700

    copied code from previous branch into new branch

--- scripts/script.js
+++ scripts/script.js
@@ -25,7 +25,7 @@ var provinces = {
 
 // regions - iso & name (country)
 var regions = {
-	"async": true,
+	"async": false,
 	"crossDomain": true,
 	"url": "https://covid-19-statistics.p.rapidapi.com/regions",
 	"method": "GET",
@@ -38,8 +38,8 @@ var regions = {
 // Get total of confirmed cases from each country and append it to total confirmed
 $.ajax(regions).done(function (regionsData) {
     var totalConfirmedCases = [];
-    for (var i = 0; i < regionsData.data.length; i++) {
-        console.log(regionsData.data[i].iso);
+    for (var i = 0; i < 50; i++) {
+       // console.log(regionsData.data[i].iso);
         var reports = {
             "async": true,
             "crossDomain": true,
@@ -78,3 +78,95 @@ $.ajax(total).done(function (response) {
 function formatNumber(num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
+
+
+//Covid Deaths by Country
+var totalConfirmedDeaths = [];
+$.ajax(regions).done(function (regionsData) {
+    var totalConfirmedCases = [];
+    for (var i = 0; i < 50; i++) {
+        
+        var reports = {
+            "async": false,
+            "crossDomain": true,
+            "url": "https://covid-19-statistics.p.rapidapi.com/reports?iso=" + regionsData.data[i].iso, 
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+                "x-rapidapi-key": "73403c6c67msha0632541172766bp194ccejsn820ec377a86a"
+            },
+        }
+        $.ajax(reports).done(function (response) {
+            
+           
+            var total = 0;
+           for (var j = 0; j < response.data.length; j++) {
+                total += response.data[j].deaths;
+                
+           }
+            try {
+                var caseObject = { name: response.data[0].region.iso, countrytotal: total }
+                    totalConfirmedDeaths.push(caseObject);
+                    
+            } catch(err) {
+                // do nothing
+             } 
+        });   
+    }
+    
+});
+
+
+
+
+totalConfirmedDeaths.sort(function (a, b) {
+    return b.countrytotal - a.countrytotal;
+    
+  });
+
+
+  async function asyncCall() {
+        for (var i = 0; i < totalConfirmedDeaths.length; i++) {
+            
+            $("#total-deaths").append("<span class='total-number-country'>" + totalConfirmedDeaths[i].countrytotal + "</span> " + totalConfirmedDeaths[i].name + "</br>")
+        }
+    }
+
+asyncCall()
+
+
+/*
+$.ajax(regions).done(function (regionsData) {
+    var totalConfirmedCases = [];
+    for (var i = 0; i < 50; i++) {
+        
+        var reports = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://covid-19-statistics.p.rapidapi.com/reports?iso=" + regionsData.data[i].iso, 
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+                "x-rapidapi-key": "73403c6c67msha0632541172766bp194ccejsn820ec377a86a"
+            },
+        }
+        $.ajax(reports).done(function (response) {
+            
+           
+            var total = 0;
+           for (var j = 0; j < response.data.length; j++) {
+                total += response.data[j].deaths;
+                console.log(response.data[j].deaths)
+           }
+            try {
+                var caseString = "";
+                caseString = total + ": " + response.data[0].region.iso;
+                totalConfirmedCases.push(caseString);
+                $("#total-deaths").append("<span class='total-number-country'>" + formatNumber(total) + "</span> " + response.data[0].region.name + "</br>")
+            } catch(err) {
+                // do nothing
+             } 
+        });   
+    }
+    
+});*/
