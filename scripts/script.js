e814ee647141b68197323471f5bdf26fe0e851e2
Nick Guimarais <Nick.guimarais@gmail.com>
2020-08-08 09:15:13 -0700

    formatted numbers with commas, updated click event for world map

--- scripts/script.js
+++ scripts/script.js
@@ -137,11 +137,15 @@ totalConfirmedDeaths.sort(function (a, b) {
 async function asyncCall() {
     for (var i = 0; i < totalConfirmedDeaths.length; i++) {
 
-        $("#total-deaths").append("<span class='total-number-country'>" + totalConfirmedDeaths[i].countrytotal + "</span> " + totalConfirmedDeaths[i].name + "</br>")
+        $("#total-deaths").append("<span class='total-number-country'>" + totalConfirmedDeaths[i].name + "</span> " + formatNumber(totalConfirmedDeaths[i].countrytotal)  + "</br>")
     }
 }
 
 asyncCall()
+
+
+
+
 
 // Show total world wide deaths
 var reports = {
@@ -210,7 +214,7 @@ $("#click-usa").click(function () {
             async function asyncCall2() {
                 for (var i = 0; i < response.locations.length; i++) {
 
-                    $("#total-cases-country").append("<span class='total-number-state'>" + response.locations[i].state + "</span> " + response.locations[i].latest.confirmed + "</br>")
+                    $("#total-cases-country").append("<span class='total-number-state'>" + response.locations[i].state + "</span> " + formatNumber(response.locations[i].latest.confirmed) + "</br>")
                 }
             }
 
@@ -235,16 +239,19 @@ $("#click-usa").click(function () {
             }
 
             asyncCall3();
+          
 
             async function asyncCall3() {
                 for (var i = 0; i < response.locations.length; i++) {
-                    $("#total-deaths").append(`<div class="input-search" data-toggle="modal" data-target="#exampleModal" data-state=${response.locations[i].state}><span class="total-number-state" >${response.locations[i].state}</span>${response.locations[i].latest.deaths}</div>`)
+                    $("#total-deaths").append(`<div class="input-search" data-toggle="modal" data-target="#exampleModal" data-state=${response.locations[i].state}><span class="total-number-state" >${response.locations[i].state}</span> ${formatNumber(response.locations[i].latest.deaths)}</div>`)
+                   
                     // $("#total-deaths").append("<div class = 'input-search' value = "+ response.locations[i].state + "> <span class='total-number-state'>" + response.locations[i].state + "</span> " + response.locations[i].latest.deaths + "</div>")
                 }
             }
         });
     }
 });
+    
 
 //code for US Map is above this line
 //Old Code --- ignore everything below here
@@ -281,5 +288,142 @@ $.ajax(regions).done(function (regionsData) {
             }
         });
     }
+
+});
+//click function for world map
+
+$(".world-map").click(function () {
+$("#total-cases-country").empty();
+$("#total-deaths").empty();
+
+$.ajax(regions).done(function (regionsData) {
+    var totalConfirmedCases = [];
+    for (var i = 0; i < 50; i++) {
+        // console.log(regionsData.data[i].iso);
+        var reports = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://covid-19-statistics.p.rapidapi.com/reports?iso=" + regionsData.data[i].iso,
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+                "x-rapidapi-key": "73403c6c67msha0632541172766bp194ccejsn820ec377a86a"
+            },
+        }
+        $.ajax(reports).done(function (response) {
+            var total = 0;
+            for (var j = 0; j < response.data.length; j++) {
+                total += response.data[j].confirmed;
+            }
+            try {
+                var caseString = "";
+                caseString = total + ": " + response.data[0].region.iso;
+                totalConfirmedCases.push(caseString);
+                $("#total-cases-country").append("<span class='total-number-country'>" + formatNumber(total) + "</span> " + response.data[0].region.name + "</br>")
+            } catch (err) {
+                // do nothing
+            }
+        });
+    }
+
+});
+
+// Display world wide number of confirmed cases
+$.ajax(total).done(function (response) {
+    // console.log(response);
+    $("#total-world-confirmed").html("Total Confirmed <br><span class='total-number'>" + formatNumber(response.data.confirmed));
+});
+
+// This function formats numbers with commas
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+regionsNonSync = {
+    "async": false,
+    "crossDomain": true,
+    "url": "https://covid-19-statistics.p.rapidapi.com/regions",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+        "x-rapidapi-key": "73403c6c67msha0632541172766bp194ccejsn820ec377a86a"
+    },
+}
+
+//Covid Deaths by Country
+var totalConfirmedDeaths = [];
+ $.ajax(regionsNonSync).done(function (regionsData) {
+    var totalConfirmedCases = [];
+    for (var i = 0; i < 50; i++) {
+
+        var reports = {
+            "async": false,
+            "crossDomain": true,
+             "url": "https://covid-19-statistics.p.rapidapi.com/reports?iso=" + regionsData.data[i].iso,
+            "method": "GET",
+             "headers": {
+                 "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+                 "x-rapidapi-key": "73403c6c67msha0632541172766bp194ccejsn820ec377a86a"
+            },
+         }
+         $.ajax(reports).done(function (response) {
+
+
+            var total = 0;
+             for (var j = 0; j < response.data.length; j++) {
+                 total += response.data[j].deaths;
+
+             }
+            try {
+                 var caseObject = { name: response.data[0].region.iso, countrytotal: total }
+                totalConfirmedDeaths.push(caseObject);
+
+             } catch (err) {
+                 // do nothing
+             }
+         });
+     }
+
+ }); 
+
+totalConfirmedDeaths.sort(function (a, b) {
+    return b.countrytotal - a.countrytotal;
+
+});
+
+
+async function asyncCall() {
+    for (var i = 0; i < totalConfirmedDeaths.length; i++) {
+
+        $("#total-deaths").append("<span class='total-number-country'>" + totalConfirmedDeaths[i].name + "</span> " + formatNumber(totalConfirmedDeaths[i].countrytotal)  + "</br>")
+    }
+}
+
+asyncCall()
+
+
+
+// Show total world wide deaths
+var reports = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://covid-19-statistics.p.rapidapi.com/reports",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+		"x-rapidapi-key": "73403c6c67msha0632541172766bp194ccejsn820ec377a86a"
+	}
+}
+
+var totalWorldDeaths = 0;
+$.ajax(reports).done(function (response) {
+  
+    for (var i = 0; i < response.data.length; i++) {
+        totalWorldDeaths += response.data[i].deaths;
+    }
+    $("#total-world-deaths").html("Total Deaths <br><span class='total-number'>" + formatNumber(totalWorldDeaths));
+
+});
+
 
 });
