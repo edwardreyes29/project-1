bf8feb92ac6d4b022b2cfb764b503bc669926cbf
Edward Reyes <reyes.edwardf@gmail.com>
2020-08-08 00:56:18 -0700

    fixed container and made it fluid

--- scripts/worldmap.js
+++ scripts/worldmap.js
@@ -23,6 +23,7 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
+svg.call(responsivefy);
 // Map and projection
 var projection = d3.geoMercator()
     .center([0, 20])                // GPS of location to zoom on
@@ -125,6 +126,33 @@ function ready(error, dataGeo) {
         .text(function (d) { return formatNumber(d) })
         .style("font-size", 10)
         .attr('alignment-baseline', 'middle')
+}
+
+function responsivefy(svg) {
+    // get container + svg aspect ratio
+    var container = d3.select(svg.node().parentNode),
+        width = parseInt(svg.style("width")),
+        height = parseInt(svg.style("height")),
+        aspect = width / height;
+
+    // add viewBox and preserveAspectRatio properties,
+    // and call resize so that svg resizes on inital page load
+    svg.attr("viewBox", "0 0 " + width + " " + height)
+        .attr("perserveAspectRatio", "xMinYMid")
+        .call(resize);
+
+    // to register multiple listeners for same event type, 
+    // you need to add namespace, i.e., 'click.foo'
+    // necessary if you call invoke this function for multiple svgs
+    // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+    d3.select(window).on("resize." + container.attr("id"), resize);
+
+    // get width of container and resize svg to fit it
+    function resize() {
+        var targetWidth = parseInt(container.style("width"));
+        svg.attr("width", targetWidth);
+        svg.attr("height", Math.round(targetWidth / aspect));
+    }
 }
 
 
